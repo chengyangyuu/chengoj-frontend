e29b27c457f2eaad8c2a06097463453c3d5608c1
kongtongxinyiji <1002797232@qq.com>
2023-10-24 17:55:37 +0800

    自定义Axios

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ onMounted(() => {
 
 const router = useRouter();
 const store = useStore();
+
 router.beforeEach((to, from, next) => {
   if (to.meta?.access === "admin") {
     if (store.state.user.loginUser?.role !== "admin") {
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,5 +1,6 @@
 import { StoreOptions } from "vuex";
 import ACCESS_ENUM from "@/access/ACCESS_ENUM";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
@@ -7,13 +8,20 @@ export default {
     namespaced: true,
     loginUser: {
       userName: "未登录",
-      userRole: ACCESS_ENUM.NOT_LOGIN,
     },
   }),
   actions: {
-    getLoginUser({ commit, state }, payload) {
+    async getLoginUser({ commit, state }, payload) {
       //改为从远程获取
-      commit("updateUser", { userName: "雍亲王" });
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: ACCESS_ENUM.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
